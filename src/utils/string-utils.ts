6c10d2029ad08251705b458b6a349981c9495c1a
Alex <58560420+AlexMA2@users.noreply.github.com>
2026-02-19 15:20:57 -0500

    chore: Remove unused functions and unused files

--- src/utils/string-utils.ts
+++ src/utils/string-utils.ts
@@ -1,7 +1,3 @@
-export function kebabToCamelCase(str: string): string {
-  return str.replace(/-([a-z])/g, (_, char) => char.toUpperCase());
-}
-
 export function kebabToPascalCase(str: string): string {
   const camel = str.replace(/-([a-z])/g, (_, char) => char.toUpperCase());
   return camel.charAt(0).toUpperCase() + camel.slice(1);
