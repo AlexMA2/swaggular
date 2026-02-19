a0102f78e3e40f0499a997d93cf2bb284412d18c
Alex <58560420+AlexMA2@users.noreply.github.com>
2026-02-19 15:33:28 -0500

    refactor: Add types to the switchTypeJson function and TSDoc

--- src/utils/build-types.ts
+++ src/utils/build-types.ts
@@ -1,23 +1,31 @@
+import { OpenAPIV3 } from 'openapi-types';
 import { interfaceState } from '../core/state/interface-state';
+import { isReference } from './type-guard';
 
-export function switchTypeJson(schema: any): string {
+/**
+ * Converts a JSON schema object to a TypeScript type string.
+ * @param schema The JSON schema object or reference object.
+ * @returns The corresponding TypeScript type.
+ */
+export function switchTypeJson(
+  schema: OpenAPIV3.ReferenceObject | OpenAPIV3.SchemaObject | undefined,
+): string {
   if (!schema || Object.keys(schema).length === 0) return 'any';
 
-  const ref = schema?.['$ref'];
-  const type = schema?.['type'];
-  const format = schema?.['format'];
-
-  if (ref) {
-    const name = ref.split('/').pop()!;
+  if (isReference(schema)) {
+    const name = schema.$ref.split('/').pop()!;
     return interfaceState.getTypeMapping(name) || name;
   }
+
+  const type = schema.type;
+  const format = schema.format;
 
   if (type === 'object') {
     return 'any';
   }
 
   if (type === 'array') {
-    return switchTypeJson(schema?.['items']) + '[]';
+    return switchTypeJson((schema as OpenAPIV3.ArraySchemaObject).items) + '[]';
   }
 
   if (type === 'number') {
