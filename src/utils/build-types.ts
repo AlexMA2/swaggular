4a39271c42afda81c5e2ba91940bee6e20e0e474
Alex <58560420+AlexMA2@users.noreply.github.com>
2026-02-19 15:25:04 -0500

    chore: Remove unused function

--- src/utils/build-types.ts
+++ src/utils/build-types.ts
@@ -1,32 +1,5 @@
 import { interfaceState } from '../core/state/interface-state';
 
-export function buildTypes(masterSchema: any, defaultType: string = 'any'): string {
-  if (!masterSchema) return defaultType;
-
-  const jsonSchema = masterSchema?.['content']?.['application/json'];
-
-  if (!jsonSchema) {
-    const formData = masterSchema?.['content']?.['multipart/form-data'];
-    if (formData) {
-      return 'FormData';
-    }
-    return defaultType;
-  }
-
-  const schema = jsonSchema?.['schema'];
-
-  if (!schema || Object.keys(schema).length === 0) return defaultType;
-
-  const ref = schema?.['$ref'];
-
-  if (ref) {
-    const name = ref.split('/').pop()!;
-    return interfaceState.getTypeMapping(name) || name;
-  }
-
-  return switchTypeJson(schema);
-}
-
 export function switchTypeJson(schema: any): string {
   if (!schema || Object.keys(schema).length === 0) return 'any';
 
